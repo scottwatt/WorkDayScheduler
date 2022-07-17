cf26b6b9d28f47a60c6b4f490b310fc25402dd67
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-17 13:57:28 -0700

    changes the hours to 9am to 5pm in html and changes local storage to reflect the html changes

--- assets/script.js
+++ assets/script.js
@@ -38,15 +38,7 @@ function displayTime() {
     localStorage.clear();
   });
 
-  $("#hour-00 .textBlock").val(localStorage.getItem("00"));
-  $("#hour-01 .textBlock").val(localStorage.getItem("01"));
-  $("#hour-02 .textBlock").val(localStorage.getItem("02"));
-  $("#hour-03 .textBlock").val(localStorage.getItem("03"));
-  $("#hour-04 .textBlock").val(localStorage.getItem("04"));
-  $("#hour-05 .textBlock").val(localStorage.getItem("05"));
-  $("#hour-06 .textBlock").val(localStorage.getItem("06"));
-  $("#hour-07 .textBlock").val(localStorage.getItem("07"));
-  $("#hour-08 .textBlock").val(localStorage.getItem("08"));
+
   $("#hour-09 .textBlock").val(localStorage.getItem("09"));
   $("#hour-10 .textBlock").val(localStorage.getItem("10"));
   $("#hour-11 .textBlock").val(localStorage.getItem("11"));
@@ -56,10 +48,3 @@ function displayTime() {
   $("#hour-15 .textBlock").val(localStorage.getItem("15"));
   $("#hour-16 .textBlock").val(localStorage.getItem("16"));
   $("#hour-17 .textBlock").val(localStorage.getItem("17"));
-  $("#hour-18 .textBlock").val(localStorage.getItem("18"));
-  $("#hour-19 .textBlock").val(localStorage.getItem("19"));
-  $("#hour-20 .textBlock").val(localStorage.getItem("20"));
-  $("#hour-21 .textBlock").val(localStorage.getItem("21"));
-  $("#hour-22 .textBlock").val(localStorage.getItem("22"));
-  $("#hour-23 .textBlock").val(localStorage.getItem("23"));
-  $("#hour-24 .textBlock").val(localStorage.getItem("24"));
